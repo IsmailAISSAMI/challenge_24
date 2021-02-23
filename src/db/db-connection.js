6199bfd69944894710e7e1f36fc9322dfa368a76
ismailaissami <aissami.ismail@gmail.com>
2021-02-23 13:27:25 +0100

    make it better

--- src/db/db-connection.js
+++ src/db/db-connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-// process.env.MONGODB_URL = 'mongodb+srv://admin:db7aresQL17dpGoW@cluster0.86pvn.mongodb.net/pomona?retryWrites=true&w=majority'
-mongoose.connect(process.env.MONGODB_URL, {
+
+mongoose.connect('mongodb+srv://admin:db7aresQL17dpGoW@cluster0.86pvn.mongodb.net/pomona?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
